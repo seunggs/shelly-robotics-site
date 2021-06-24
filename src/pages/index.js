5a8257c79700aa45ea2ad5f44f9673c529956228
Seungchan Lee <seunggs@gmail.com>
2021-06-23 19:32:55 -0700

    update messaging

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,11 +8,17 @@ export default function Home() {
   return (
     <Layout fullscreen={true} header={false} footer={false}>
       <div className="bg-spectrum h-screen">
-        <Link to="/">
-          <div className="p-4">
-            <img src={logo} alt="Logo" style={{ width: '128px' }} />
-          </div>
-        </Link>
+        <div className="flex justify-between items-center">
+          <Link to="/">
+            <div className="p-4">
+              <img src={logo} alt="Logo" style={{ width: '128px' }} />
+            </div>
+          </Link>
+
+          <Link to="/about">About</Link>
+          <Link to="/demo">Demo</Link>
+          {/* <Link to="/blog">Blog</Link> */}
+        </div>
 
         <div className="grid grid-cols-12 items-center h-full">
 
@@ -22,7 +28,8 @@ export default function Home() {
               <Headline size="xl" color="white" className="mb-7 font-medium">Shelly Robotics</Headline>
 
               <div className="opacity-70">
-                <p className="text-2xl mb-2">End-to-end AI robotics for warehouse pick and pack</p>
+                <p className="text-2xl mb-2">We make AI robotics research and experiments easy</p>
+                <p className="text-2xl mb-2">Train and test your model on real robots - minus the hassle</p>
                 <p className="text-2xl mb-5">(More details coming soon!)</p>
               </div>
             </div>
